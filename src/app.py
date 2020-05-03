aba83b8e27d81f34043533f7a505e586ea7605b9
Anubhav Y <contact+code@anubhav.codes>
2020-05-03 20:33:55 +0200

    👩‍🌾Add a handler for trello webhooks.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello World!"
+
+
+@app.route("/trello")
+def trello_webhook():
+    return jsonify({"message": "okay"})
 
 
 if __name__ == "__main__":
