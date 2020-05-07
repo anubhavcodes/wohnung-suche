54df3dd51215734da168b13bb6c050285f68e97c
Anubhav Y <contact+code@anubhav.codes>
2020-05-07 10:31:34 +0200

    🎠 Add cli for docker run.

---
+++ src/cli.py
@@ -0,0 +1,23 @@
+from pprint import pprint
+from urllib.parse import urlparse
+
+import click
+from app import PROVIDERS
+from utils import validate_url
+
+
+@click.command()
+@click.argument("url")
+def scrape(url):
+    if not validate_url(url):
+        click.echo(pprint({"message": f"Invalid url: {url}"}))
+    try:
+        result = PROVIDERS[urlparse(url).netloc](url=url).scrape()
+        if result:
+            click.echo(pprint(result))
+    except Exception as e:
+        raise (e)
+
+
+if __name__ == "__main__":
+    scrape()
